9d90f8de09f0dec3b77b1aa3ff89752ab8f9851f
Harun Günlü <gue@christmann.info>
2018-02-19 09:33:39 +0100

    POST Request Prototype (dont work)

--- info.php
+++ info.php
@@ -1,11 +1,27 @@
 <?php
+//$_POST;
+//    $response = [
+//        'date' =>  strftime("%D"),
+//        'day' =>  strftime("%A"),
+//        'month' => strftime("%B"),
+//        'year' => strftime("%G"),
+//    ];
 $_POST;
-    $response = [
-        'date' =>  strftime("%D"),
-        'day' =>  strftime("%A"),
-        'month' => strftime("%B"),
-        'year' => strftime("%G"),
-    ];
+if($datei== 1){
+    $response = [output =>  strftime("%D")];
+}
+
+if($datei == 2){
+    $response = [output =>  strftime("%A")];
+}
+
+if($datei == 3){
+    $response = [output =>  strftime("%B")];
+}
+
+if($datei== 4){
+    $response = [output => strftime("%G")];
+}
 
 $json =json_encode($response);
 echo $json;
--- lib.js
+++ lib.js
@@ -1,31 +1,31 @@
-function callDate() {
+function callData(datei) {
     $.post('info.php', function (data) {
         var response = JSON.parse(data);
         var date = response.date;
         $('#demo').html(date);
 })
 }
-function callDay() {
-    $.post('info.php', function (data) {
-        var response = JSON.parse(data);
-        var day = response.day;
-        $('#demo').html(day);
-    })
-}
-function callMonth() {
-    $.post('info.php', function (data) {
-        var response = JSON.parse(data);
-        var month = response.month;
-        $('#demo').html(month);
-    })
-}
-function callYear() {
-    $.post('info.php', function (data) {
-        var response = JSON.parse(data);
-        var year = response.year;
-        $('#demo').html(year);
-    })
-}
+// function callDay() {
+//     $.post('info.php', function (data) {
+//         var response = JSON.parse(data);
+//         var day = response.day;
+//         $('#demo').html(day);
+//     })
+// }
+// function callMonth() {
+//     $.post('info.php', function (data) {
+//         var response = JSON.parse(data);
+//         var month = response.month;
+//         $('#demo').html(month);
+//     })
+// }
+// function callYear() {
+//     $.post('info.php', function (data) {
+//         var response = JSON.parse(data);
+//         var year = response.year;
+//         $('#demo').html(year);
+//     })
+// }
 
 
 
